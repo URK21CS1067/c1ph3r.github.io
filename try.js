0a0e785b5076f1f9e1a961f3272e87c032b9b8c6
URK21CS1067 <93753029+URK21CS1067@users.noreply.github.com>
2024-02-16 01:15:54 +0530

    Update try.js

--- try.js
+++ try.js
@@ -1,8 +1,8 @@
 const PHP_SELF = window.location.pathname;
 
-if (/config\.php\/?$/i.test(PHP_SELF)) {
+if (/config\.js\/?$/i.test(PHP_SELF)) {
   document.body.textContent = "That's all you got, try even more harder :)";
-  throw new Error("PHP_SELF ends with '/config.php'");
+  throw new Error("PHP_SELF ends with '/config.js'");
 }
 
 if (new URLSearchParams(window.location.search).has('source')) {
