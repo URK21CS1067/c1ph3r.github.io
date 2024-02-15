b514af7332377d6b11513d37c415f98e95c5c0ad
URK21CS1067 <93753029+URK21CS1067@users.noreply.github.com>
2024-02-16 00:36:55 +0530

    Update try.js

--- try.php
+++ try.php
@@ -1,23 +1,27 @@
-<?php
-include 'config.php'; // FLAG is defined in config.php
+// Assuming 'config.js' defines FLAG similarly to 'config.php'
+import { FLAG } from './config.js';
 
-if (preg_match('/config\.php\/*$/i', $_SERVER['PHP_SELF'])) {
-  exit("Thats's all you got, try even more harder :)");
+// Simulating PHP's $_SERVER['PHP_SELF']
+const PHP_SELF = window.location.pathname;
+
+if (/config\.php\/*$/i.test(PHP_SELF)) {
+  document.body.textContent = "Thats's all you got, try even more harder :)";
+  window.stop();
 }
 
-if (isset($_GET['source'])) {
-  highlight_file(basename($_SERVER['PHP_SELF']));
-  exit();
+if (new URLSearchParams(window.location.search).has('source')) {
+  // In JavaScript, we can't highlight the file as PHP does, but we can redirect to the file itself.
+  window.location.href = PHP_SELF.split('/').pop();
 }
 
-$secret = bin2hex(random_bytes(64));
-if (isset($_POST['guess'])) {
-  $guess = (string) $_POST['guess'];
-  if (hash_equals($secret, $guess)) {
-    $message = 'The flag is: ' . FLAG;
+const secret = window.crypto.getRandomValues(new Uint8Array(64)).reduce((acc, val) => acc + val.toString(16), '');
+
+if (new FormData(document.querySelector('form')).has('guess')) {
+  const guess = new FormData(document.querySelector('form')).get('guess');
+  if (secret === guess) { // hash_equals is not necessary in JS as === does not suffer from timing attacks
+    const message = 'The flag is: ' + FLAG;
   } else {
-    $message = 'NICE TRY!!!';
+    const message = 'NICE TRY!!!';
   }
 }
-?>
 
