ab7a0bc00428a2280374c47421f05e903b56a987
URK21CS1067 <93753029+URK21CS1067@users.noreply.github.com>
2024-02-16 00:50:13 +0530

    Update try.js

--- try.js
+++ try.js
@@ -1,27 +1,32 @@
-// Assuming 'config.js' defines FLAG similarly to 'config.php'
 import { FLAG } from './config.js';
 
-// Simulating PHP's $_SERVER['PHP_SELF']
 const PHP_SELF = window.location.pathname;
 
-if (/config\.php\/*$/i.test(PHP_SELF)) {
-  document.body.textContent = "Thats's all you got, try even more harder :)";
+if (/config\.js\/?$/i.test(PHP_SELF)) {
+  document.body.textContent = "That's all you got, try even harder :)";
   window.stop();
 }
 
 if (new URLSearchParams(window.location.search).has('source')) {
-  // In JavaScript, we can't highlight the file as PHP does, but we can redirect to the file itself.
   window.location.href = PHP_SELF.split('/').pop();
 }
 
-const secret = window.crypto.getRandomValues(new Uint8Array(64)).reduce((acc, val) => acc + val.toString(16), '');
+const secret = Array.from(window.crypto.getRandomValues(new Uint8Array(64)), val => val.toString(16)).join('');
 
-if (new FormData(document.querySelector('form')).has('guess')) {
-  const guess = new FormData(document.querySelector('form')).get('guess');
-  if (secret === guess) { // hash_equals is not necessary in JS as === does not suffer from timing attacks
-    const message = 'The flag is: ' + FLAG;
-  } else {
-    const message = 'NICE TRY!!!';
-  }
+const form = document.querySelector('form');
+if (form) {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    if (formData.has('guess')) {
+      const guess = formData.get('guess');
+      let message;
+      if (secret === guess) {
+        message = 'The flag is: ' + FLAG;
+      } else {
+        message = 'NICE TRY!!!';
+      }
+      document.body.textContent = message;
+    }
+  });
 }
-
