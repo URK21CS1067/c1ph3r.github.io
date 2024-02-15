552284b0c8e04ada478dea6cd3f3b89f7eafb77f
URK21CS1067 <93753029+URK21CS1067@users.noreply.github.com>
2024-02-16 01:04:08 +0530

    Update try.js

--- try.js
+++ try.js
@@ -1,14 +1,14 @@
-// Check if the URL contains the 'source' query parameter
-const urlParams = new URLSearchParams(window.location.search);
-if (urlParams.has('source')) {
-  // Perform actions specific to when the 'source' parameter is present
-  // For example, display the source code of the page
-  fetch(window.location.href)
-    .then(response => response.text())
-    .then(data => {
-      // Display the source code in an alert box (you can customize this as needed)
-      alert('Page Source:\n\n' + data);
-    });
+// Simulating PHP's $_SERVER['PHP_SELF']
+const PHP_SELF = window.location.pathname;
+
+if (/config\.js\/?$/i.test(PHP_SELF)) {
+  document.body.textContent = "That's all you got, try even more harder :)";
+  throw new Error("PHP_SELF ends with '/config.php'");
+}
+
+if (new URLSearchParams(window.location.search).has('source')) {
+  // In JavaScript, we can't highlight the file as PHP does, but we can redirect to the file itself.
+  window.location.href = PHP_SELF.split('/').pop();
 }
 
 const secret = Array.from(window.crypto.getRandomValues(new Uint8Array(64)), val => val.toString(16)).join('');
@@ -21,7 +21,7 @@ if (form) {
     if (formData.has('guess')) {
       const guess = formData.get('guess');
       let message;
-      if (secret === guess) {
+      if (secret === guess) { // hash_equals is not necessary in JS as === does not suffer from timing attacks
         message = 'The flag is: ' + FLAG;
       } else {
         message = 'NICE TRY!!!';
