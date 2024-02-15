c99302782fc64dfd8dfb5b886faf61c25242b27f
URK21CS1067 <93753029+URK21CS1067@users.noreply.github.com>
2024-02-16 00:56:10 +0530

    Update try.js

--- try.js
+++ try.js
@@ -1,14 +1,14 @@
-import { FLAG } from './config.js';
-
-const PHP_SELF = window.location.pathname;
-
-if (/config\.js\/?$/i.test(PHP_SELF)) {
-  document.body.textContent = "That's all you got, try even harder :)";
-  window.stop();
-}
-
-if (new URLSearchParams(window.location.search).has('source')) {
-  window.location.href = PHP_SELF.split('/').pop();
+// Check if the URL contains the 'source' query parameter
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.has('source')) {
+  // Perform actions specific to when the 'source' parameter is present
+  // For example, display the source code of the page
+  fetch(window.location.href)
+    .then(response => response.text())
+    .then(data => {
+      // Display the source code in an alert box (you can customize this as needed)
+      alert('Page Source:\n\n' + data);
+    });
 }
 
 const secret = Array.from(window.crypto.getRandomValues(new Uint8Array(64)), val => val.toString(16)).join('');
